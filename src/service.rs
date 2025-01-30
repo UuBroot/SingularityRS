419a2ab88d63d4bc0ada5a034fc9cd2b07801ee5
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-01-30 15:49:29 +0100

    added service.rs file for doing the convertion and checking for errors in input and output files

--- src/service.rs
+++ src/service.rs
@@ -1,3 +1,53 @@
+mod files;
+mod ffmpeg_module;
+
+use std::path::Path;
+use crate::service::files::get_absolute_path;
+
 pub fn convert(input: &str, output: &str) {
     println!("converting {} to {}", input, output);
+
+    //gets absolute path for input
+    let abs_input = match get_absolute_path(input){
+        Some(abs_path)=>{
+            println!("Absolute path: {}", abs_path);
+            abs_path
+        },
+        None => {
+            println!("File does not exist or cannot be resolved.");
+            "".parse().unwrap()
+        }
+    };
+    println!("{}", abs_input);
+
+    //gets absolute path for output
+    let abs_output = match get_absolute_path(output){
+        Some(abs_path)=>{
+            println!("Absolute path: {}", abs_path);
+            abs_path
+        },
+        None => {
+            println!("File does not exist or cannot be resolved.");
+            "".parse().unwrap()
+        }
+    };
+    println!("{}", abs_output);
+
+    let mut output_folder_split: Vec<_> = abs_output.split('/').collect();
+    output_folder_split.pop();//removes the filename from the output
+    let output_folder = output_folder_split.join("/");
+
+    //Checks if input file exists
+    if !Path::new(input).exists() {
+        println!("{} doesn't exist", input);
+        return;
+    }
+
+    //Checks if the output folder exists
+    if !Path::new(&output_folder).exists() {
+        println!("{} folder doesn't exist", output);
+        return;
+    }
+
+
 }
