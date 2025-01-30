31b25c1e61bc61b27cbd9a6f988a697053cc806c
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-01-30 15:49:44 +0100

    added function to get absolute path in files.rs

---
+++ src/service/files.rs
@@ -0,0 +1,10 @@
+use std::fs::canonicalize;
+use std::path::Path;
+
+pub fn get_absolute_path(path: &str) -> Option<String> {
+    let path = Path::new(path);
+    match canonicalize(path) {
+        Ok(abs_path) => Some(abs_path.to_string_lossy().into_owned()),
+        Err(_) => None,
+    }
+}
