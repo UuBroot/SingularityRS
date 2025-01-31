68792d6506bf9e13a44e5dcac1ac780cf5e81c12
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-01-31 23:31:46 +0100

    added function to create empty file to files.rs

--- src/service/files.rs
+++ src/service/files.rs
@@ -1,4 +1,4 @@
-use std::fs::canonicalize;
+use std::fs::{canonicalize, File};
 use std::path::Path;
 
 pub fn get_absolute_path(path: &str) -> Option<String> {
@@ -7,4 +7,7 @@ pub fn get_absolute_path(path: &str) -> Option<String> {
         Ok(abs_path) => Some(abs_path.to_string_lossy().into_owned()),
         Err(_) => None,
     }
+}
+pub fn make_empty_file(path: &str) {
+    File::create(path).unwrap();
 }
