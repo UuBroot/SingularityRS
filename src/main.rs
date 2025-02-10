06617b49c4cdd8694e1e3510502be0e240f5ab15
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-02-10 10:17:40 +0100

    added gui_service support

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,17 @@ mod module;
 mod files;
 mod service;
 use service::convert;
+use crate::gui_service::start_gui;
+
 mod gui_service;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let input_path: String = args[1].clone();
-    let output_path: String = args[2].clone();
-    convert(&*input_path, &*output_path);
+    if args.len() == 1 {
+        println!("opening gui");
+        start_gui();
+    }else if args.len() == 3 {
+        convert(&*args[1].clone(), &*args[2].clone());
+    }else {
+        println!("Usage: {} input_file.mp4 output_file.mp3", args[0]);
+    }
 }
