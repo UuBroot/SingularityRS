48cf4569a600fad572dab74b6acc4dfc0d6466c2
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-02-10 09:47:26 +0100

    made main.rs code smaller

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,5 @@ mod service;
 use service::convert;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let input_path: String = args[1].clone();
-    let output_path: String = args[2].clone();
-    convert(&*input_path, &*output_path);
+    convert(&*args[1].clone(), &*args[2].clone());
 }
